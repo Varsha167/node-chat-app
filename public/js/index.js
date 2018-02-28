befc75a160276433aa84b7857dd8c3ce33837893
Varsha167 <varsha167@gmail.com>
2018-02-28 15:18:06 +0530

    Add chat app

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io()
+
+socket.on('connect' ,()=>{ //variablesocket is already there. so no socket argument required
+  console.log("Connected to server")
+})
+
+socket.emit('createMessage' , {
+  to:'varsha',
+  text:'Welcome to Yale'
+})
+
+socket.on('disconnect', ()=>{
+  console.log("Disconnected from server")
+})
+
+socket.on('newMessage', function(chat) {
+  console.log("new Message" , chat)
+})
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path') //built in. no need to install
+const http = require('http') //built in. no need to install
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 
@@ -7,9 +9,34 @@ const port = process.env.PORT || 3000
 
 var app = express()
 
+var server = http.createServer(app)
+var io = socketIO(server)
 //will let you see the static index.html file
 app.use(express.static(publicPath))
 
-app.listen(port, (req,res)=>{
+//io lets you listen to the event
+io.on('connection' , (socket)=>{ //this socket represents individual socket
+  console.log("New user conncted")
+
+  socket.emit('newMessage' , { //emitting to client
+    from: 'Varsha',
+    text: 'I got into Yale!',
+    createdAt: '22'
+  })
+
+  socket.on('createMessage' , (createnewchat)=>{
+    console.log(createnewchat) //Listening from client
+  })
+
+  socket.on('disconnect', ()=>{
+    console.log("User Disconnected")
+  })
+})
+
+
+
+
+
+server.listen(port, (req,res)=>{
   console.log(`Listening on port ${port}`)
 })
